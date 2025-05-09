a2e31a96203b51a110e3921514e5d105e9c3e6c6
Tanguygab <tanguy@gabril.com>
2025-05-09 16:25:24 +0200

    Added cors, bcrypt & multer to avoid issues with CORS, hash password & receive files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors())
 
 // Connexion MongoDB
 mongoose.connect(process.env.MONGO_URI, {
