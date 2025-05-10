247c4faac1d2d11c83fea49fdfd368395e509e96
Tanguygab <tanguy@gabril.com>
2025-05-10 18:32:54 +0200

    added route to get user by id

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import User from '../models/user.js'
+import { error } from '../utils.js'
 const router = Router();
 
 router.get('/', (req, res) => { res.json({ message: 'GET all users' }); });
-router.get('/:id', (req, res) => { res.json({ message: `GET user by ID: ${req.params.id}` }); });
-router.post('/', (req, res) => { res.json({ message: 'CREATE user' }); });
+router.get('/:id', async (req, res) => { 
+    const user = await User.findOne({ _id: req.params.id }).populate("role").populate("subscription")
+    if (!user) {
+        error(res, "user.not-found", 404)
+        return
+    }
+    res.json(user)
+});
 router.put('/:id', (req, res) => { res.json({ message: `UPDATE user ID: ${req.params.id}` }); });
 router.delete('/:id', (req, res) => { res.json({ message: `DELETE user ID: ${req.params.id}` }); });
 
