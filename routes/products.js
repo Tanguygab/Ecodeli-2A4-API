122f26d54d117455ca33fdc6993a11b1a7edff03
Tanguygab <tanguy@gabril.com>
2025-05-11 18:02:45 +0200

    Added /api/products/sellers & simple /api/products

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 const router = Router();
+import User from '../models/user.js'
+import { searchQuery } from '../utils.js';
 
-router.get('/', (req, res) => { res.json({ message: 'GET all products' }); });
+router.get('/', (req, res) => {
+    res.json([{id: 0, name: "Peluche Creeper", price: 5, size: {id: 0, name: "small", size: 5}, seller: {name: "Hi"}}]);
+});
+router.get('/sellers', async (req, res) => {
+    res.json(await User.find({$or: [searchQuery(req, "input"), searchQuery(req, "input", "firstname")]}, {_id: 1, firstname: 1, name: 1}))
+})
 router.get('/:id', (req, res) => { res.json({ message: `GET product by ID: ${req.params.id}` }); });
 router.post('/', (req, res) => { res.json({ message: 'CREATE product' }); });
 router.put('/:id', (req, res) => { res.json({ message: `UPDATE product ID: ${req.params.id}` }); });
