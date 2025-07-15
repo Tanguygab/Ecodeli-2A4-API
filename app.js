c22665636b3096b67e112205874d7b2d96fa6353
Tmelchio <163995515+Tmelchio@users.noreply.github.com>
2025-07-16 00:28:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,18 @@ import cors from "cors";
 const app = express();
 const router = express.Router()
 
-// Middleware
+
 app.use(json());
 app.use(cors());
 app.use(express.static('public'))
 app.use('/uploads', express.static('uploads'))
 app.use("/api", router)
 
-// MongoDB Connection
 connect(`mongodb://db:${process.env.DB_PORT}/${process.env.DB_NAME}`)
   .then(() => {
     console.log("✅ MongoDB connected")
 
-    // Server Launch
+   
     const PORT = process.env.PORT;
     app.listen(PORT, () => console.log(`🌐 Server launched on http://localhost:${PORT}`));
 
@@ -26,14 +25,14 @@ connect(`mongodb://db:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 
 import fs from 'fs';
 
-// Mongo Schemas
+
 fs.readdir("./models", (err, files) => {
   for (const file of files) {
     import(`./models/` + file)
   }
 })
 
-// API Routes
+
 fs.readdir("./routes", async (err, files) => {
   for (const file of files) {
     router.use("/" + file.substring(0, file.length - 3), (await import(`./routes/` + file)).default)
