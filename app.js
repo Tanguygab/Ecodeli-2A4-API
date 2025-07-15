0252f9a4ee0c5d2d5607bc94778402ed09710290
Tanguygab <tanguy@gabril.com>
2025-07-15 14:46:36 +0200

    Removed unused dotenv dependency

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import express, { json } from "express";
 import { connect } from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 const router = express.Router()
