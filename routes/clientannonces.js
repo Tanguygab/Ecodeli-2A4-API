ccb7293a7e99135f1d8aec76557d4dbccb93af7b
Tmelchio <163995515+Tmelchio@users.noreply.github.com>
2025-07-16 13:02:30 +0200

    Update clientannonces.js
    erreur de frappe

--- routes/clientannonces.js
+++ routes/clientannonces.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import ClientAnnonce from '../models/clientannonces.js';
-import { isAuthenticated } from '../middleware/auth.js'; 
+import { isAuthenticated } from '../mmiddleware/auth.js'; 
 
 const router = express.Router();
 
